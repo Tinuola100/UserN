8c2285af489fca1ddecbc357bb2876ae8dfce320
Tinuola100 <lowatinuola@gmail.com>
2022-04-14 07:32:24 -0700

    assignment18

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const a = 2;
+const b = 3;
+
+console.log(a + b)
+
+//question1
+
+var firstN = "hannah";
+var lastN = "lowa";
+
+var firstName = firstN.slice(0,4);
+var lastName = lastN.slice(0,1);
+
+
+console.log(firstName + lastName)
+ 
+//question2
+
+
